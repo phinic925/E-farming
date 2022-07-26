197e5991727fe83566b9d01b96825923afb4a66a
phinic925 <phinicnyangate480@gmail.com>
2022-07-26 12:24:12 +0100

    create components

---
+++ src/components/Customer.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Customer(){
+    return(
+        <>
+        </>
+    )
+
+}
+export default Customer;
---
+++ src/components/CustomerForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CustomerForm(){
+    return(
+        <>
+        </>
+    )
+}
+export default CustomerForm;
