2b0d005a245f7b8f5a956e20bb86c278ffa11069
phinic925 <phinicnyangate480@gmail.com>
2022-07-26 16:51:29 +0100

    style navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
         <Link to="/" exact className="navbar-brand"> Home </Link>
         <div className="collapse navbar-collapse">
-        <ul className="navbar-nav" >
+        <ul className="navbar-nav " >
         <li className="navbar-item">
         <Link exact to="/products" className="navbar-brand"> products </Link>
         </li>
