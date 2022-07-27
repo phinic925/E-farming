c86467b6cee095ae171eeb0e848e4417eec33f9d
phinic925 <phinicnyangate480@gmail.com>
2022-07-27 13:16:50 +0100

    add the farmer section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import React from "react";
 function Home(){
     return(
         <>
-        <h1 className="heading"> RiverLands </h1>
-        <p> Welcome to RiverLands!
-     <br/> This a platform whereby you can sell your farm products and customers will view and order.<br/>
-          You can also upload your farm products to the website.<br/> We really care for farmers around the country since without farmers, we will have no food.
-        </p>
+        <h1 className="heading"> Mkulima Halisi </h1>
+        <p> Welcome to Mkulima Halisi! </p>
+
+        
+    
         </>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ function Navbar(){
     return(
         
         <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
-        <Link to="/" exact className="navbar-brand"> Home </Link>
+        <Link to="/" exact className="navbar-brand"> All products </Link>
         <div className="collapse navbar-collapse">
         <ul className="navbar-nav " >
         <li className="navbar-item">
         <Link exact to="/products" className="navbar-brand"> products </Link>
         </li>
         <li className="navbar-item">
-        <Link exact to="/customer" className="navbar-brand"> customer </Link>
+        <Link exact to="/customer" className="navbar-brand"> Farmer </Link>
         </li>
         </ul>
         </div>
