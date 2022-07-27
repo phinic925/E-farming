ae9ee4d1e3925e32b6c88c7b3de02ca4c9510189
phinic925 <phinicnyangate480@gmail.com>
2022-07-27 14:16:16 +0100

    adds search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,26 @@
 import React from "react";
 
+
+
 function Home(){
     return(
         <>
         <h1 className="heading"> Mkulima Halisi </h1>
-        <p> Welcome to Mkulima Halisi! </p>
+        <p className="para"> Welcome to Mkulima Halisi! </p>
+        <div className="input-group col-xs-4">
+      <input
+        type="search"
+        placeholder="Search Product"
+        className="form-control"
+       
+      />
+     <button className ="btn btn-primary"> search </button>
+      
+      
+    </div>
 
         
-    
+        
         </>
     )
 }
