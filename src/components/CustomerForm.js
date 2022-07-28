af2fe5665294f81dcfde448c8fcb6c7daf6966ec
phinic925 <phinicnyangate480@gmail.com>
2022-07-28 11:53:46 +0100

    adds form

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React,{useState}from "react";
 
 function FarmerForm(){
+    const[name,setName] = useState("");
+    const[image,setImage_url] = useState("");
+    const[description,setDescription] = useState("");
+    const[category,setCategory] = useState("");
+  
+    function handleSubmit(e){
+      e.preventDefault();
+      const itemData = {
+        name: name,
+        image_url: image_url,
+        description: description,
+        category: category,
+  
+      };
+      fetch(url,{
+        method: "POST",
+        headers:{"Content-Type": "application/json"},
+        body:JSON.stringify(itemData),
+      })
+      .then(res=> res.json())
+    .then(data=>{
+      console.log(data)
+    })
+}
     return(
         <form className="form-horizontal">
            <div className="form-group">
